d4539a9b99e8f9f715db7389ac3b13888d1d41e2
samiraelshon123 <samiraelshon872@gmail.com>
2023-02-17 00:34:51 +0200

    first commit

--- resources/views/Invoices/invoices.blade.php
+++ resources/views/Invoices/invoices.blade.php
@@ -38,7 +38,7 @@
 					<div class="my-auto">
 						<div class="d-flex">
                             <h4 class="content-title mb-0 my-auto"><a href="{{ route('home') }}">الرئيسيه</a></h4><span class="text-muted mt-1 tx-13 mr-2 mb-0">/
-                    اضافة فاتورة</span>
+                    الفواتبر </span>
 						</div>
 					</div>
 
